cd5c2993f8065ad6c183d45b89563185cce3d959
AbhinavMaddirala <67218672+AbhinavMaddirala@users.noreply.github.com>
2020-08-02 20:07:52 +0530

    Add files via upload

---
+++ compare2num/src/CompareTwoNum.java
@@ -0,0 +1,16 @@
+public class CompareTwoNum {
+
+    public static void main(String args[]){
+       // declare two variables
+        int number1 = 105;
+        int number2 = 75;
+        if ( number1 > number2){
+            System.out.println (number1+"is greater than "+ number2);
+        }else if (number1 < number2){
+            System.out.println(number1 +" is less than "+number2);
+        }else {
+            System.out.println(number1+"equal to"+number2);
+        }
+
+    }
+}
